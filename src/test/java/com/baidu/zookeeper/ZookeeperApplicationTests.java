b256254f374520cdaa756376d6baf64eba3b395d
QaYs4910 <QaYs4910@126.com>
2023-02-22 11:12:52 +0800

    zookeeper 连接

--- src/test/java/com/baidu/zookeeper/ZookeeperApplicationTests.java
+++ src/test/java/com/baidu/zookeeper/ZookeeperApplicationTests.java
@@ -1,13 +1,31 @@
 package com.baidu.zookeeper;
 
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
 
 @SpringBootTest
 class ZookeeperApplicationTests {
 
+    /**
+     * 客户端连接zookeeper
+     * @throws IOException
+     */
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        String connectString = "CentOSA:2181,CentOSB:2181,CentOSC:2181";
+        int sessionTimeout =5000;
+        ZooKeeper zooKeeper = new ZooKeeper(connectString, sessionTimeout, new Watcher() {
+            @Override
+            public void process(WatchedEvent watchedEvent) {
+                System.out.println("执行..");
+            }
+        });
+        System.out.println("完毕.."+zooKeeper);
     }
 
 }
